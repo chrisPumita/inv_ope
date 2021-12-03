0ddebe3511a9a45678b9344067c8e4c627c64d19
Christan RCSG <christian.floppy@gmail.com>
2021-12-03 17:41:09 -0600

    datos

--- source.js
+++ source.js
@@ -1,23 +1,21 @@
 anychart.onDocumentReady(function () {
-
-    // create data
     // create data
     var data = {
     nodes: [
-        {id: "Richard"},
-        {id: "Larry"},
-        {id: "Marta"},
-        {id: "Jane"},
-        {id: "Norma"},
-        {id: "Frank"},
-        {id: "Brett"},
-        {id: "Tommy"}
+        {id: "A",last_name: "A"},
+        {id: "B",last_name: "B"},
+        {id: "C",last_name: "C"},
+        {id: "D",last_name: "D"},
+        {id: "E",last_name: "E"},
+        {id: "F",last_name: "F"},
+        {id: "G",last_name: "G"},
+        {id: "H",last_name: "H"}
     ],
     edges: [
-        {from: "Richard", to: "Larry"},
-        {from: "Richard", to: "Marta"},
-        {from: "Larry",   to: "Marta"},
-        {from: "Marta",   to: "Jane",
+        {from: "A", to: "B", distance:5},
+        {from: "A", to: "C", distance:5},
+        {from: "C",   to: "D", distance:5},
+        {from: "B",   to: "E", distance:5,
         normal: {stroke:  {
                             color: "#ffa000",
                             thickness: "2",
@@ -34,22 +32,46 @@ anychart.onDocumentReady(function () {
         },
         selected: {stroke: "4 #ffa000"}
         },
-        {from: "Jane",    to: "Norma"},
-        {from: "Jane",    to: "Frank"},
-        {from: "Jane",    to: "Brett"},
-        {from: "Brett",   to: "Frank"}
+        {from: "B",    to: "F", distance:5},
+        {from: "C",    to: "G", distance:5},
+        {from: "G",    to: "B", distance:5},
+        {from: "G",   to: "H", distance:5}
     ]
     };
 
     // create a chart and set the data
     var chart = anychart.graph(data);
-
+    loadDataTable(data);
+  
     var zoomController = anychart.ui.zoom();
     zoomController.target(chart);
     zoomController.render();
+
+    chart.nodes().labels().enabled(true);
+
+// configure labels of nodes
+chart.nodes().labels().format("{%id}");
+chart.nodes().labels().fontSize(12);
+chart.nodes().labels().fontWeight(600);
     // set the container id
     chart.container("container");
 
     // initiate drawing the chart
     chart.draw();
   });
+
+  function loadDataTable(data) {
+    let nodos = data.nodes;
+    let arcos = data.edges;
+    let template = "";
+    for (let i = 0; i < nodos.length; i++) {
+        template += `<tr>
+        <th scope="row">${i}</th>
+        <td>${nodos[i].id}</td>
+        <td>${arcos[i].from}</td>
+        <td>${arcos[i].to}</td>
+        <td>${arcos[i].distance} cm</td>
+      </tr>`;
+    }
+    $("#tblNodos").html(template);
+  }
